1a4536cd7ecf8428d672540f5d958224e0f80b5a
Petr Tripolsky <tripolskypetr@gmail.com>
2024-04-17 20:21:08 +0500

    better-logger

--- index.mjs
+++ index.mjs
@@ -3,34 +3,93 @@
 import { createProxyMiddleware } from "http-proxy-middleware";
 import { createRequire } from 'module'
 
-import "babel-polyfill";
-
 import https from 'https';
 import http from 'http';
 import path from 'path';
+import fs from 'fs';
 
 import express from "express";
 import cors from "cors";
 import nocache from 'nocache';
 import jwt from "jsonwebtoken";
-import { default as anywhere } from "express-cors-anywhere";
+import pino from 'pino';
+
+const portLogger = pino({
+    transport: {
+        target: 'pino/file',
+        options: { destination: 'error.txt' }
+    },
+    formatters: {
+        log: (obj) => ({
+            _SOURCE: "PORT",
+            ...obj
+        }),
+    }
+});
+
+const proxyLogger = pino({
+    transport: {
+        target: 'pino/file',
+        options: { destination: 'error.txt' }
+    },
+    formatters: {
+        log: (obj) => ({
+            _SOURCE: "PROXY",
+            ...obj
+        }),
+    }
+});
+
+const corsLogger = pino({
+    transport: {
+        target: 'pino/file',
+        options: { destination: 'error.txt' }
+    },
+    formatters: {
+        log: (obj) => ({
+            _SOURCE: "CORS",
+            ...obj
+        }),
+    }
+});
+
 
 const require = createRequire(import.meta.url);
 
 const args = process.argv.slice(2);
-const [configPath = "./sample/pwalauncher.js"] = args;
+const [configPath = "./pwalauncher.config.js"] = args;
+
+if (!fs.existsSync(configPath)) {
+    console.log(`${path.resolve(process.cwd(), configPath)} not found`);
+    console.log('Prese read the manual: https://github.com/react-declarative/pwalauncher');
+    console.log();
+    process.exit(-1);
+}
 
 const config = require(configPath);
 config.port = config.port ?? 80;
 
 const app = express();
 
-console.log(anywhere)
-
 app.use(nocache());
 app.use(cors());
-app.use(express.static(path.join(process.cwd(), 'wwwroot')));
-app.use("/cors-anywhere", anywhere.default());
+
+app.use("/cors/", async (req, res) => {
+    const url = req.url.replace('/', '');
+    corsLogger.info(`RESTREAM url=${url} date=${new Date().toString()}`);
+    try {
+        const response = await fetch(url);
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        if (!buffer.length) {
+            throw new Error('empty buffer')
+        }
+        res.status(200).send(buffer);
+    } catch (error) {
+        corsLogger.error(`RESTREAM url=${url}`, error);
+        res.status(200).json({ error: true });
+    }
+});
 
 if (config.jwtSecret) {
     config.ports?.forEach((port) => {
@@ -52,19 +111,47 @@ if (config.jwtSecret) {
 
 config.ports?.forEach((port) => {
     app.use(
-        `/${port}`,
+        `/${port}/*`,
         createProxyMiddleware({
             target: `http://127.0.0.1:${port}`,
             changeOrigin: true,
             ws: true,
-            pathRewrite: (path) => {
-                return path.replace(`/${port}`, '');
+            logger: portLogger,
+            pathRewrite: {
+                [`^/${port}/`]: '/',
             },
+            onError: (err, req, res) => {
+                portLogger.warn(err);
+                res.status(500).json({
+                    error: true,
+                });
+            }
         })
     );
 });
 
-app.get("*", function (_, res) {
+config.proxy?.forEach(({ path, link }) => {
+    const endpoint = `/${path}`;
+    app.use(
+        endpoint,
+        createProxyMiddleware({
+            target: link.replace(endpoint, ''),
+            changeOrigin: true,
+            ws: true,
+            logger: proxyLogger,
+            onError: (err, req, res) => {
+                proxyLogger.warn(err);
+                res.status(500).json({
+                    error: true,
+                });
+            }
+        })
+    );
+});
+
+app.use(express.static(path.join(process.cwd(), 'wwwroot')));
+
+app.get("*", (_, res) => {
     res.sendFile(path.join(process.cwd(), "./wwwroot/index.html"));
 });
 
--- sample/pwalauncher.config.js
+++ sample/pwalauncher.config.js
@@ -13,6 +13,12 @@ module.exports = {
         8089,
         8090,
     ],
+    proxy: [
+        {
+            path: 'v1',
+            link: 'http://127.0.0.1:8080/v1/'
+        }
+    ],
     ssl: {
         key: fs.readFileSync('./ssl/test.key', 'utf8'),
         cert: fs.readFileSync('./ssl/test.crt', 'utf8')
